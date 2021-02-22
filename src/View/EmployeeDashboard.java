390f90a84db2b7238a7563ca67e65c1d910a30a6
Dylan Tellei <dylantellei@gmail.com>
2021-02-21 21:48:56 -0500

    employee dashboard view

---
+++ src/View/EmployeeDashboard.java
@@ -0,0 +1,17 @@
+/**
+ * Insert necessary imports for creating view via desired method i.e. javaFX
+ */
+package View;
+
+
+public class EmployeeDashboard {
+    /**
+    * Button layout information and labels go here
+    * Sample Button Implementation for this class might include:
+    * Retrieve Customer Data
+    * Modify Loan Information
+    * Print Loan Information
+    * Logout
+    * 
+    */
+}
