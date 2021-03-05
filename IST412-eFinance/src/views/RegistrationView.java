c4986a828847914b34c981c67ffd47b74f3697bb
Jared Rice <60943441+jmr4349@users.noreply.github.com>
2021-03-05 17:02:40 -0500

    Add files via upload

---
+++ IST412-eFinance/src/views/RegistrationView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views;
+
+/**
+ *
+ * @author Jared
+ */
+public class RegistrationView {
+    
+    
+        private void signUp(String email, String password, String firstName, String lastName, int age) {
+        
+    }
+}
